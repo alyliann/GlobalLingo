c8bf8144a6bacba8040bd44d4742923e4ad22554
Alysa Vega <alyliann0325@gmail.com>
2023-07-11 19:28:42 +0000

    Changed pseudocode "Button" to SubmitField

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, Button, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
-class HomeForm(FlaskForm):
-    redirect_button = Button('Go To Translate')
+'''class HomeForm(FlaskForm):
+    redirect_button = Button('Go To Translate')'''
 
 class TranslateForm(FlaskForm):
     text_input = TextAreaField('Enter text to translate',
                                validators=[DataRequired(), Length(min=1, max=10000)])
     language = StringField('Enter a language to translate to',
                            validators=[DataRequired()])
-    translate_text = Button('Translate')
+    translate_text = SubmitField('Translate')
