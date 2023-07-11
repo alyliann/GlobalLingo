c19a0773bf9ee6c3d4cebdacad7d8dd80bb07440
Alysa Vega <92405765+alyliann@users.noreply.github.com>
2023-07-11 11:11:40 -0400

    Undo last commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import git
 import requests
 from forms import RegistrationForm
@@ -7,34 +8,25 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from googletrans import Translator
 
 app = Flask(__name__)
+proxied = FlaskBehindProxy(app)  ## add this line
 translator = Translator()
 proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = '3f8742cae18a7f0af440fe7979f95617'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
-
+@@ -23,13 +26,22 @@ def __repr__(self):
 with app.app_context():
     db.create_all()
 
+@app.route("/")
+def hello_world():
 @app.route("/home")
 def about():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
 
-<<<<<<< HEAD:main.py
-@app.route("/translator")
+@app.route("/about")
 def about():
-   return render_template('translator.html', subtitle='Translator', text='This is the translator')
-=======
+    return render_template('about.html', subtitle='About Page', text='This is the about page')
 
 @app.route("/translator", methods= ['GET','POST'])
 def translator():
@@ -47,28 +39,6 @@ def translator():
     else:
         return render_template("translator.html")
 
->>>>>>> 82bdf4a229ec981dc69116a5df8ea183fc155b2a:app.py
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit(): # checks if entries are valid
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
-    return render_template('register.html', title='Register', form=form)
-
-@app.route("/update_server", methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('/home/babynameoriginfinder/SEO-Week-3')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
