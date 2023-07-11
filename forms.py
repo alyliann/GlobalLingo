7eb22ac9af6f8672ae2b02bcd320b3d0c09e33b9
Alysa Vega <alyliann0325@gmail.com>
2023-07-11 19:03:37 +0000

    Created form classes and field variables

--- forms.py
+++ forms.py
@@ -2,13 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class InputForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-    # two buttons, one submit, one translate (optional swap)
+class HomeForm(FlaskForm):
+    redirect_button = Button('Go To Translate')
+
+class TranslateForm(FlaskForm):
+    text_input = TextAreaField('Enter text to translate')
+    language = StringField('Enter a language to translate to')
+    translate_text = Button('Translate')
