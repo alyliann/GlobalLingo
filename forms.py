df98f5f5b5b65194b4458a6a453d775bd63b93b3
Alysa Vega <alyliann0325@gmail.com>
2023-07-11 19:10:52 +0000

    Added validators and updated imports

--- forms.py
+++ forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, Button, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class HomeForm(FlaskForm):
     redirect_button = Button('Go To Translate')
 
 class TranslateForm(FlaskForm):
-    text_input = TextAreaField('Enter text to translate')
-    language = StringField('Enter a language to translate to')
+    text_input = TextAreaField('Enter text to translate',
+                               validators=[DataRequired(), Length(min=1, max=10000)])
+    language = StringField('Enter a language to translate to',
+                           validators=[DataRequired()])
     translate_text = Button('Translate')
